65d0b1a095735b7f4da9cbb3a16b9fb7c6917ca1
JM Bae <jungman82@gmail.com>
2023-06-23 15:14:14 +0900

    DetailView, ResultView 클래스 뷰 변경

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,10 +1,10 @@
 import logging
-import datetime
-from django.http import HttpResponseRedirect
-from django.urls import reverse
-from django.shortcuts import render, get_object_or_404
 
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views import generic
+
 from polls.models import Question, Choice
 
 logger = logging.getLogger(__name__)
@@ -19,17 +19,14 @@ class IndexView(generic.ListView):
         return Question.objects.all().order_by('-pub_date')[:5]
 
 
-# Create your views here.
-def index(request):
-    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    # latest_question_list = [{'question_text': 'Hello, World', 'pub_date': datetime.datetime.now()}]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 
 def vote(request, question_id):
